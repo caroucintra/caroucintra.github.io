8afeda934e1f5265a0b3e0057b633af8dbc81e9f
caroucintra <up201906845@fe.up.pt>
2024-10-02 15:03:44 +0100

    Rearranged portfolio

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -7,17 +7,17 @@ import cognispace from "../../assets/cognispace.png"
 const Menu = [
     {
         id: 1,
-        image: vota,
-        title: "Vota!",
-        category: "Projects",
-        link: "https://github.com/parlamento-org/parlamento-app"
-    },
-    {
-        id: 2,
         image: emote1,
         title: "eMote",
         category: "School",
         link: "https://github.com/caroucintra/PI_PCAs"
+    },
+    {
+        id: 2,
+        image: cognispace,
+        title: "CogniSpace",
+        category: "School",
+        link: "https://github.com/caroucintra/cognispace"
     },
     {
         id: 3,
@@ -28,18 +28,18 @@ const Menu = [
     },
     {
         id: 4,
-        image: cognispace,
-        title: "CogniSpace",
-        category: "School",
-        link: "https://github.com/caroucintra/cognispace"
-    },
-    {
-        id: 5,
         image: terracota,
         title: "terracota",
         category: "Creative",
         link: "https://www.instagram.com/terracota.shop"
-    }
+    },    
+    {
+        id: 5,
+        image: vota,
+        title: "Vota!",
+        category: "Projects",
+        link: "https://github.com/parlamento-org/parlamento-app"
+    },
 ]
 
 export default Menu
